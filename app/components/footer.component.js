98748cac13fa601e9075d186d39a32f227c00fa7
confused1108 <ahuja.hits812@yahoo.com>
2018-01-14 18:05:04 +0530

    Initial commit

---
+++ app/components/footer.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="footer">
+          <h3>Created with Love</h3>
+      </div>
+    )
+  }
+
+}
+
+export default Footer
